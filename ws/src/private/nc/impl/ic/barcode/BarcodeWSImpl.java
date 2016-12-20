a9344556d082ab16626b793839b6b8005a1191b0
mjh <mojiahui@qq.com>
2016-12-20 14:00:25 +0800

    条码接口定义

--- ws/src/private/nc/impl/ic/barcode/BarcodeWSImpl.java
+++ ws/src/private/nc/impl/ic/barcode/BarcodeWSImpl.java
@@ -8,10 +8,49 @@ import nc.pub.ic.barcode.FreeMarkerUtil;
 public class BarcodeWSImpl implements IBarcodeWS {
 
 	@Override
-	public String getProductInfoByCode(String code) {
+	public String GetProductInfoByCode(String productCode) {
 		HashMap<String, Object> para = new HashMap<String, Object>();
-		para.put("code", code);
-		return FreeMarkerUtil.process(para, "nc/config/ic/barcode/material.fl");
+		para.put("code", productCode);
+		return FreeMarkerUtil.process(para,"nc/config/ic/barcode/material.fl");
+	}
+	@Override
+	public String GetProductionOrderByNo(String orderNo) {
+		return null;
+	}
+
+	@Override
+	public String PostGoodsReceiveNote(String xml) {
+		return null;
+	}
+
+	@Override
+	public String GetDeliveryNoteByNo(String transationType, String orderNo) {
+		return null;
+	}
+
+	@Override
+	public String PostDeliveryNoteDetailScanQty(String xml) {
+		return null;
+	}
+
+	@Override
+	public String PostOtherDeliveryNoteDetailActualQty(String xml) {
+		return null;
+	}
+
+	@Override
+	public String PostTransferReceiveNote(String xml) {
+		return null;
+	}
+
+	@Override
+	public String PostTransferOutNote(String xml) {
+		return null;
+	}
+
+	@Override
+	public String PostTransferInNote(String xml) {
+		return null;
 	}
 
 }
--- ws/src/public/nc/ift/ic/barcode/IBarcodeWS.java
+++ ws/src/public/nc/ift/ic/barcode/IBarcodeWS.java
@@ -7,6 +7,66 @@ public interface IBarcodeWS {
 	 * @param code
 	 * @return
 	 */
-	public String getProductInfoByCode(String code);
+	public String GetProductInfoByCode(String productCode);
+	
+	/**
+	 * 读取生产订单
+	 * @param orderNo
+	 * @return
+	 */
+	public String GetProductionOrderByNo(String orderNo);
+	
+	/**
+	 * 写入成品入库，根据来源的生产订单，生成完工报告和产成品入库
+	 * @param xml
+	 * @return
+	 */
+	public String PostGoodsReceiveNote(String xml);
+	
+	/**
+	 * 读取出库单，类型包括销售出库，其他出库和调拨出库
+	 * @param transationType
+	 * @param orderNo
+	 * @return
+	 */
+	public String GetDeliveryNoteByNo(String transationType, String orderNo);
+	
+	/**
+	 * 回写出库单扫码数量
+	 * @param xml
+	 * @return
+	 */
+	public String PostDeliveryNoteDetailScanQty(String xml);
+	
+	
+	/**
+	 * 回写出库单实发数量
+	 * @param xml
+	 * @return
+	 */
+	public String PostOtherDeliveryNoteDetailActualQty(String xml);
+	
+	
+	/**
+	 * 写入调拨入库
+	 * @param xml
+	 * @return
+	 */
+	public String PostTransferReceiveNote(String xml);
+	
+	/**
+	 * 写入转库和其他出库
+	 * @param xml
+	 * @return
+	 */
+	public String PostTransferOutNote(String xml);
+	
+	/**
+	 * 写入转库的其他入库
+	 * @param xml
+	 * @return
+	 */
+	public String PostTransferInNote(String xml);
+	
 	
 }
