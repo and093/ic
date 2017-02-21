c7399f5b09883cdc6c834a6e66d741c1eef6c5fc
mojiahui0769@126.com <mojiahui0769@126.com>
2017-02-21 11:02:18 +0800

    增加脚本

---
+++ basedoc/script/all/业务处理.sql
@@ -0,0 +1,105 @@
+
+insert into pub_pluginitem
+  (pk_pluginitem,
+   Vmodulename,
+   Vcomponentname,
+   Vextendpointname,
+   Veventtype,
+   Vextendtype,
+   vruleclass)
+values
+  ('0001MJ20170206000001',
+   'IC',               
+   'm4c',
+   'nc.impl.ic.m4c.action.UpdateAction',   
+   'before',                    
+   'addBefore',           
+   'nc.rule.ic.barcode.BeforeUpdateRule');
+   
+insert into pub_pluginitem
+  (pk_pluginitem,
+   Vmodulename,
+   Vcomponentname,
+   Vextendpointname,
+   Veventtype,
+   Vextendtype,
+   vruleclass)
+values
+  ('0001MJ20170206000002',
+   'IC',               
+   'm4c',
+   'nc.impl.ic.m4c.action.DeleteAction',   
+   'before',                    
+   'addBefore',           
+   'nc.rule.ic.barcode.BeforeDeleteRule');   
+   
+   
+insert into pub_pluginitem
+  (pk_pluginitem,
+   Vmodulename,
+   Vcomponentname,
+   Vextendpointname,
+   Veventtype,
+   Vextendtype,
+   vruleclass)
+values
+  ('0001MJ20170206000003',
+   'IC',               
+   'm4y',
+   'nc.impl.ic.m4y.action.UpdateAction',   
+   'before',                    
+   'addBefore',           
+   'nc.rule.ic.barcode.BeforeUpdateRule');
+   
+insert into pub_pluginitem
+  (pk_pluginitem,
+   Vmodulename,
+   Vcomponentname,
+   Vextendpointname,
+   Veventtype,
+   Vextendtype,
+   vruleclass)
+values
+  ('0001MJ20170206000004',
+   'IC',               
+   'm4y',
+   'nc.impl.ic.m4y.action.DeleteAction',   
+   'before',                    
+   'addBefore',           
+   'nc.rule.ic.barcode.BeforeDeleteRule');   
+   
+   
+
+insert into pub_pluginitem
+  (pk_pluginitem,
+   Vmodulename,
+   Vcomponentname,
+   Vextendpointname,
+   Veventtype,
+   Vextendtype,
+   vruleclass)
+values
+  ('0001MJ20170206000005',
+   'IC',               
+   'm4i',
+   'nc.impl.ic.m4i.action.UpdateAction',   
+   'before',                    
+   'addBefore',           
+   'nc.rule.ic.barcode.BeforeUpdateRule');
+   
+insert into pub_pluginitem
+  (pk_pluginitem,
+   Vmodulename,
+   Vcomponentname,
+   Vextendpointname,
+   Veventtype,
+   Vextendtype,
+   vruleclass)
+values
+  ('0001MJ20170206000006',
+   'IC',               
+   'm4i',
+   'nc.impl.ic.m4i.action.DeleteAction',   
+   'before',                    
+   'addBefore',           
+   'nc.rule.ic.barcode.BeforeDeleteRule');     
